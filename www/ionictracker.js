edc4de9eceb4913a56374ffc9aa1a2a9a33901f6
shimyon.parmar <shimyon.parmar@scrumbees.com>
2021-03-25 12:55:17 +0530

    . gitignore is now working

--- www/ionictracker.js
+++ www/ionictracker.js
@@ -4,24 +4,24 @@ exports.coolMethod = function(arg0, success, error) {
     exec(success, error, pluginname, 'coolMethod', [arg0]);
 };
 
-module.exports.add = function(arg0, success, error) {
+exports.add = function(arg0, success, error) {
     //const x = cordova(this, 'add', {}, [{ param1: num1, param2: num2 }]);
     exec(success, error, pluginname, 'add', [arg0]);
 };
 
-module.exports.startTracking = function(arg0, success, error) {
+exports.startTracking = function(arg0, success, error) {
     //const x = cordova(this, 'add', {}, [{ param1: num1, param2: num2 }]);
     exec(success, error, pluginname, 'startTracking', [arg0]);
 };
 
-module.exports.stopTracking = function(arg0, success, error) {
+exports.stopTracking = function(arg0, success, error) {
     exec(success, error, pluginname, 'stopTracking', [arg0]);
 };
 
-module.exports.getStatus = function(arg0, success, error) {
+exports.getStatus = function(arg0, success, error) {
     exec(success, error, pluginname, 'getStatus', [arg0]);
 };
 
-module.exports.requestToLocationService = function(arg0, success, error) {
+exports.requestToLocationService = function(arg0, success, error) {
     exec(success, error, pluginname, 'requestToLocationService', [arg0]);
 };
